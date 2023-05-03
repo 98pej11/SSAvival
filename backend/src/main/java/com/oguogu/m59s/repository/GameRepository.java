510dc9c432f47fe8e540c5d412a6af0bd36d8bb4
김형균 <heng2926@naver.com>
2023-05-03 13:35:00 +0900

    FEAT : REST API 1차 완성
    consist of game, minigame, minigamedetail
    
    RESOLVES:

--- backend/src/main/java/com/oguogu/m59s/repository/GameRepository.java
+++ backend/src/main/java/com/oguogu/m59s/repository/GameRepository.java
@@ -1,12 +1,12 @@
 package com.oguogu.m59s.repository;
 
-import com.oguogu.m59s.entity.User;
+import com.oguogu.m59s.entity.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface GameRepository extends JpaRepository<Game, Long> {
+    List<Game> findAllByUserId(long userId);
 }
