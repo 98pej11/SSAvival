0ede16532a63ac181a70ce8860e1bffceb47b35a
김형균 <heng2926@naver.com>
2023-05-10 08:53:21 +0900

    UPDATE : merge 중 발생한 에러 수정
    설정 파일과 중복되는 파일 수정 및 삭제
    
    RESOLVES :

--- backend/src/main/java/com/oguogu/m59s/model/service/UserService.java
+++ backend/src/main/java/com/oguogu/m59s/model/service/UserService.java
@@ -8,11 +8,9 @@ import java.util.List;
 public interface UserService {
     UserDto detailUser(long userId);
     List<UserDto> listUser();
-<<<<<<< Updated upstream
-=======
 //    UserDto findUserByNickname(String nickname);
     UserDto findUserByEmail(String email);
     public void modifyUser(UserDto userDto);
     void registUser(UserDto userDto);
->>>>>>> Stashed changes
+
 }
