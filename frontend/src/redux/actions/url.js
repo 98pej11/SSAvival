f1c11cbf1654f34a956759ff8d50f24550ea8e08
김형균 <heng2926@naver.com>
2023-05-15 08:51:27 +0900

    FEATURE : REDIS user list 출력에 캐시 적용
    랭킹을 가져오는 작업이 연산이 많이 들어가서 30분마다 reload 하는 형식으로 캐시활용
    
    RESOLVES :

--- frontend/src/redux/actions/url.js
+++ frontend/src/redux/actions/url.js
@@ -1,5 +1,5 @@
 export const baseUrl = `http://localhost:8084/api`;
-export const kakaoUrl = `http://localhost:8083`;
+export const kakaoUrl = `http://localhost:3000`;
 
 // export const baseUrl = `https://k8a602.p.ssafy.io/api`;
 // export const kakaoUrl = `https://k8a602.p.ssafy.io`;
