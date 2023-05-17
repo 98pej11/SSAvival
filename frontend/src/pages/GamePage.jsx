b0eb944aca244a74302534bbae52dc02490cfe8b
양현용 <9935ok9938@gmail.com>
2023-05-17 10:16:31 +0900

    UPDATE : GamePage

--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -52,6 +52,7 @@ const gameContainer2 = {
   border: "none", // 테두리 없애기
   borderRadius: 10,
   boxShadow: "0px 0px 3px 2px rgba(0,0,0,0.2)", // 그림자 추가하기
+  backgroundColor: "rgba(255, 255, 255, 0.7)", // 배경색 투명하게 만들기
   padding: 3,
   maxWidth: "40%", // 최대 너비 값 설정
   width: "100%",
