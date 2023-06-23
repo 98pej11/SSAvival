bb5c727e118856f974ace485acb494bc9232ed9f
moqoru <june6362@naver.com>
2023-06-23 10:04:27 +0900

    CHORE: AWS, KAKAO KEY 정보 삭제

--- frontend/src/components/KakaoLoginData.jsx
+++ frontend/src/components/KakaoLoginData.jsx
@@ -1,5 +1,5 @@
 import { kakaoUrl } from "../redux/actions/url";
-export const REST_API_KEY = "0fe168522b983b72237c2dfd782649a2";
+export const REST_API_KEY = "REST_API_KEY";
 export const REDIRECT_URI = `${kakaoUrl}/user/kakao/check`;
 export const LOGOUT_REDIRECT_URI = `${kakaoUrl}`;
-export const APP_ADMIN_KEY = "88e9e1cb167c912c097712c984f0e282";
+export const APP_ADMIN_KEY = "APP_ADMIN_KEY";
